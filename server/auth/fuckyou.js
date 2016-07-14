353fabd63c1049b33d3df4c7b5d6cacf10603cd5
Jason Wang <jasonwang67@gmail.com>
2016-07-14 11:16:39 -0400

    lolol

---
+++ server/auth/fuckyou.js
@@ -0,0 +1,6 @@
+module.exports =
+  {
+  clientID: '238524570915-ivf9lnhm9bsfq13cle5ap8s28d4lmhrp.apps.googleusercontent.com',
+  clientSecret: 'GST6VQnVmhx1YIB1vDXXB3PF',
+  callbackURL: '/auth/google/callback'
+}
--- server/auth/google.oauth.js
+++ server/auth/google.oauth.js
@@ -3,12 +3,10 @@
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var User = require('../api/users/user.model');
+var fuckyou = require('./fuckyou');
 
-module.exports = new GoogleStrategy({
-  clientID: '238524570915-ivf9lnhm9bsfq13cle5ap8s28d4lmhrp.apps.googleusercontent.com',
-  clientSecret: 'GST6VQnVmhx1YIB1vDXXB3PF',
-  callbackURL: '/auth/google/callback'
-}, function (token, refreshToken,  profile, triggerSerializationOfUser) {
+
+module.exports = new GoogleStrategy(fuckyou, function (token, refreshToken,  profile, triggerSerializationOfUser) {
   // this only runs when somebody logs in through google
   User.findOrCreate({
     where: {googleId: profile.id},
